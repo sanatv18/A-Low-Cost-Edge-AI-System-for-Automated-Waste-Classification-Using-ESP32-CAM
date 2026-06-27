6414f19448a89c1a5ba68439ad99438f624c1038
Sanat vasisht <sanatv1718@gmail.com>
2026-06-27 11:19:34 +0530

    Update classifier.h

--- firmware/classifier.h
+++ firmware/classifier.h
@@ -1,1 +1,45 @@
+/******************************************************************************
+ * File        : classifier.h
+ * Project     : Edge AI & IoT Smart Waste Classification System
+ * Description : Edge Impulse Inference Engine
+ ******************************************************************************/
 
+#ifndef CLASSIFIER_H
+#define CLASSIFIER_H
+
+#include <Arduino.h>
+
+/*----------------------------------------------------
+                    Waste Types
+----------------------------------------------------*/
+
+enum WasteType
+{
+    PAPER,
+    BATTERY,
+    UNKNOWN
+};
+
+/*----------------------------------------------------
+                Detection Result
+----------------------------------------------------*/
+
+struct DetectionResult
+{
+    WasteType type;
+    String label;
+
+    float confidence;
+
+    bool detected;
+};
+
+/*----------------------------------------------------
+                Public Functions
+----------------------------------------------------*/
+
+bool initializeClassifier();
+
+DetectionResult runInference();
+
+#endif
