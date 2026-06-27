10c7f5fc63945087bd0129351015d71291b96537
Sanat vasisht <sanatv1718@gmail.com>
2026-06-27 11:18:20 +0530

    Update camera.h

--- firmware/camera.h
+++ firmware/camera.h
@@ -1,1 +1,28 @@
+/******************************************************************************
+ * File        : camera.h
+ * Project     : Edge AI & IoT Smart Waste Classification System
+ * Description : ESP32-CAM Driver
+ ******************************************************************************/
 
+#ifndef CAMERA_MANAGER_H
+#define CAMERA_MANAGER_H
+
+#include "esp_camera.h"
+
+bool initializeCamera();
+
+void deinitializeCamera();
+
+bool captureImage(
+    uint32_t width,
+    uint32_t height,
+    uint8_t *buffer
+);
+
+int getCameraData(
+    size_t offset,
+    size_t length,
+    float *out_ptr
+);
+
+#endif
