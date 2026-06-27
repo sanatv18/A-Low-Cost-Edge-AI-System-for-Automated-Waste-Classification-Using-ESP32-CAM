d24f3d171fe8c33b40183c59b329d5f8e2ea9ec3
Sanat vasisht <sanatv1718@gmail.com>
2026-06-27 11:17:03 +0530

    Update display.h

--- firmware/display.h
+++ firmware/display.h
@@ -1,1 +1,30 @@
+/******************************************************************************
+ * File        : display.h
+ * Project     : Edge AI & IoT Smart Waste Classification System
+ * Description : OLED Display Manager
+ ******************************************************************************/
 
+#ifndef DISPLAY_MANAGER_H
+#define DISPLAY_MANAGER_H
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+bool initializeDisplay();
+
+void clearScreen();
+
+void showWelcomeScreen();
+
+void showBootScreen();
+
+void showPrediction(
+    String label,
+    float confidence
+);
+
+void showCloudStatus(bool uploaded);
+
+void showError(String message);
+
+#endif
